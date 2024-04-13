04fbb1232b1a766c93e91a06bca612c9237ac202
YuvRaj6392 <74025229+YuvRaj6392@users.noreply.github.com>
2024-04-13 23:05:46 +0530

    added private router added bunch of window reload to mimic real financial application

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 export default function Header() {
     const history=useNavigate()
+    const token=localStorage.getItem("token")
+    useEffect(()=>{
+      if(!token){
+       window.location.href="/signin"
+      }
+    },[token])
   return (
     <div className='flex justify-between items-center shadow-lg p-2 sm:p-4'>
       <div className='text-2xl font-extrabold sm:text-4xl cursor-pointer' onClick={()=>{
@@ -15,7 +21,7 @@ export default function Header() {
         </div>
         <div onClick={()=>{
           localStorage.removeItem("token")
-          window.location.reload()
+          window.location.href="/signin"
         }} className='text-xl font-normal sm:text-2xl bg-red-700 py-1 px-2 text-white rounded-md cursor-pointer'>
           Logout 
         </div>
